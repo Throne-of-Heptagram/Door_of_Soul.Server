0c493339f86583bc41777971d247b7354b578dd9
DSobscure <snbruce13@yahoo.com.tw>
2017-08-02 15:23:21 +0800

    Setup initial template

---
+++ Door_of_Soul.Server.Test/DeviceFactoryUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Door_of_Soul.Server.Test
+{
+    [TestClass]
+    public class DeviceFactoryUnitTest
+    {
+        [TestMethod]
+        public void CreateDevice1()
+        {
+            ServerDevice device = DeviceFactory.Instance.CreateDevice(null);
+            Assert.IsNotNull(device);
+            Assert.IsTrue(DeviceFactory.Instance.ContainsDevice(device.Guid));
+        }
+        [TestMethod]
+        public void RemoveDevice1()
+        {
+            ServerDevice device = DeviceFactory.Instance.CreateDevice(null);
+            Assert.IsTrue(DeviceFactory.Instance.ContainsDevice(device.Guid));
+            Assert.IsTrue(DeviceFactory.Instance.RemoveDevice(device.Guid));
+        }
+    }
+}
---
+++ Door_of_Soul.Server/ServerDevice.cs
@@ -0,0 +1,22 @@
+﻿using Door_of_Soul.Core;
+using System;
+using Door_of_Soul.Communication.Infrastructure.Server.Device;
+
+namespace Door_of_Soul.Server
+{
+    public class ServerDevice : Device
+    {
+        public Guid Guid { get; private set; }
+        public DeviceCommunicationService CommunicationService { get; private set; }
+
+        public ServerDevice(Guid guid, DeviceCommunicationInterface communicationInterface) : base()
+        {
+            Guid = guid;
+            CommunicationService = new DeviceCommunicationService(this, communicationInterface);
+        }
+        public override string ToString()
+        {
+            return $"Device{Guid}";
+        }
+    }
+}
